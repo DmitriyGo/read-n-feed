e46bce5d80230ce32f71f8cbe34685705fa1f9f9
Yarre <yaroslav.syvukha@gmail.com>
2025-01-18 22:03:03 +0100

    fix: colour palette

--- apps/web/src/components/common/layout/index.tsx
+++ apps/web/src/components/common/layout/index.tsx
@@ -1,25 +1,12 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import { ModalManager } from '../modal-manager';
-
-import { Route } from '@/constants';
+import { Header } from './header';
 
 export const Layout = () => {
   return (
     <>
-      <nav className="w-full bg-card py-4 text-white">
-        <ul className="flex w-full justify-evenly flex-row gap-4">
-          <li>
-            <Link to={Route.Home}>Home</Link>
-          </li>
-          <li>
-            <Link to={Route.SignIn}>Sign In</Link>
-          </li>
-          <li>
-            <Link to={Route.SignUp}>Sign Up</Link>
-          </li>
-        </ul>
-      </nav>
+      <Header />
 
       <ModalManager />
 
