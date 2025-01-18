60d2e0d2ebe8ebccfdd1d410b6860cbdf1b56c13
Yarre <yaroslav.syvukha@gmail.com>
2025-01-18 21:06:23 +0100

    feat: add dialog component and integrate it into the home page; update TypeScript configuration and package dependencies

---
+++ apps/web/src/store/modal-store.tsx
@@ -0,0 +1,32 @@
+import { Dispatch, ReactElement } from 'react';
+import { create } from 'zustand';
+
+type ModalModeInfo = {
+  component: ReactElement;
+  width?: number;
+  height?: number;
+};
+
+const ModalInfos = {
+  SignIn: {
+    component: <p>SignIn</p>,
+  },
+  SignUp: {
+    component: <p>SingUp</p>,
+  },
+} satisfies Record<string, ModalModeInfo>;
+
+export type ModalMode = keyof typeof ModalInfos;
+
+type ModalStore = {
+  mode: ModalModeInfo | null;
+  setMode: Dispatch<ModalMode | null>;
+};
+
+export const useModalStore = create<ModalStore>((set) => ({
+  mode: null,
+  setMode: (value) =>
+    set({
+      mode: value ? ModalInfos[value] : null,
+    }),
+}));
