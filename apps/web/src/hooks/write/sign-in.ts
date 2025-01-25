46536f0b29f25850f6c14a8907735cd1091f78d1
Yarre <yaroslav.syvukha@gmail.com>
2025-01-25 17:56:42 +0100

    feat: implement access token management and logout functionality

--- apps/web/src/hooks/write/sign-in.ts
+++ apps/web/src/hooks/write/sign-in.ts
@@ -1,15 +1,17 @@
 import { LoginDto } from '@read-n-feed/application';
 import { useMutation } from '@tanstack/react-query';
 
-import { ApiRoute } from '@/constants/api-routes';
-import { env } from '@/env';
+import { ACCESS_TOKEN, ApiRoute } from '@/constants';
 import { axiosSecure } from '@/lib';
 
 export const useSignIn = () => {
   return useMutation({
     mutationFn: async (data: LoginDto) => {
-      console.log('data', env.VITE_API_URL);
-      await axiosSecure.post(ApiRoute.Auth.Login, data);
+      const response = await axiosSecure.post<{
+        accessToken: string;
+      }>(ApiRoute.Auth.Login, data);
+
+      localStorage.setItem(ACCESS_TOKEN, response.data.accessToken);
     },
     onError: (error) => {
       console.error(error);
--- apps/web/src/hooks/write/sign-up.ts
+++ apps/web/src/hooks/write/sign-up.ts
@@ -1,13 +1,14 @@
 import { RegisterDto } from '@read-n-feed/application';
+import { User } from '@read-n-feed/domain';
 import { useMutation } from '@tanstack/react-query';
 
-import { ApiRoute } from '@/constants/api-routes';
+import { ApiRoute } from '@/constants';
 import { axiosSecure } from '@/lib';
 
 export const useSignUp = () => {
   return useMutation({
     mutationFn: async (data: RegisterDto) => {
-      await axiosSecure.post(ApiRoute.Auth.Register, data);
+      await axiosSecure.post<User>(ApiRoute.Auth.Register, data);
     },
     onError: (error) => {
       console.error(error);
